f663b821f1c9e7daeda6eb263d88fa897b0d25d6
zeris <bxjc1live@hotmail.com>
2021-06-24 23:19:03 -0500

    Agregada gráfica de gantt de ejemplo en inicio así como información sobre la página

--- src/views/inicio.js
+++ src/views/inicio.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Form, Button } from '../components/basiccomponents';
+import { Form } from '../components/basiccomponents';
 import SweetAlert from 'react-bootstrap-sweetalert';
 import { NavBar } from '../components/navbar';
+import { Chart } from 'react-google-charts';
 import '../css/styles.css';
 
 export default class Inicio extends React.Component {
@@ -32,20 +33,44 @@ export default class Inicio extends React.Component {
 						"typeDate": "text",
 						"required": true,
 						"maxLength": 7,
-						"colValue": 12
+						"colValue": 6
+					},
+					{
+						"id": "tbCorreo",
+						"tag": "TextBox",
+						"placeholder": "correo@dominio.com",
+						"text": "Correo electrónico",
+						"typeDate": "email",
+						"required": true,
+						"maxLength": 20,
+						"colValue": 6
 					}
 				],
 				[
 					{
 						"id": "tbPassword",
 						"tag": "TextBox",
-						"placeholder": "123ASD5.",
+						"placeholder": "Debe incluir al menos una letra mayuscula, número y caráter especial",
 						"text": "Contraseña",
 						"typeDate": "password",
 						"required": true,
 						"maxLength": 10,
-						"colValue": 6
+						"colValue": 12
+					}
+				],
+				[
+					{
+						"id": "tbPassword2",
+						"tag": "TextBox",
+						"placeholder": "",
+						"text": "Repite tu contraseña",
+						"typeDate": "password",
+						"required": true,
+						"maxLength": 10,
+						"colValue": 12
 					},
+				],
+				[
 					{
 						"id": "tbNombre",
 						"tag": "TextBox",
@@ -54,7 +79,7 @@ export default class Inicio extends React.Component {
 						"typeDate": "name",
 						"required": true,
 						"maxLength": 20,
-						"colValue": 6
+						"colValue": 12
 
 					}
 				],
@@ -91,38 +116,8 @@ export default class Inicio extends React.Component {
 						"typeDate": "telephone",
 						"required": true,
 						"maxLength": 20,
-						"colValue": 3
+						"colValue": 12
 					},
-					{
-						"id": "tbNumeroEntero",
-						"tag": "TextBox",
-						"placeholder": "12",
-						"text": "Número entero",
-						"typeDate": "int",
-						"required": true,
-						"maxLength": 20,
-						"colValue": 3
-					},
-					{
-						"id": "tbNumeroFlotante",
-						"tag": "TextBox",
-						"placeholder": "12.34",
-						"text": "Número flotante",
-						"typeDate": "float",
-						"required": true,
-						"maxLength": 20,
-						"colValue": 3
-					},
-					{
-						"id": "tbCorreo",
-						"tag": "TextBox",
-						"placeholder": "correo@dominio.com",
-						"text": "Correo electrónico",
-						"typeDate": "email",
-						"required": true,
-						"maxLength": 20,
-						"colValue": 3
-					}
 				]
 			]
 		return campos;
@@ -135,7 +130,7 @@ export default class Inicio extends React.Component {
 					{
 						"id": "tbUsername",
 						"tag": "TextBox",
-						"placeholder": "Usuario de prueba",
+						"placeholder": "",
 						"text": "Nombre de usuario",
 						"typeDate": "text",
 						"required": true,
@@ -147,7 +142,7 @@ export default class Inicio extends React.Component {
 					{
 						"id": "tbPassword",
 						"tag": "TextBox",
-						"placeholder": "123ASD5.",
+						"placeholder": "",
 						"text": "Contraseña",
 						"typeDate": "password",
 						"required": true,
@@ -167,14 +162,6 @@ export default class Inicio extends React.Component {
 				"sectionItems": [
 					{
 						"text": "inicio",
-						"type": "link"
-					},
-					{
-						"text": "Communities",
-						"type": "link"
-					},
-					{
-						"text": "Nosotros",
 						"type": "link"
 					},
 					{
@@ -245,7 +232,7 @@ export default class Inicio extends React.Component {
 	onClickRegistro(event, obj) {
 		this.setState({
 			flotante: <SweetAlert
-				title={"Inicio de sesión"}
+				title={"Crear tu cuenta"}
 				onCancel={this.onClickCancelPopUp}
 				showConfirm={false}
 				closeAnim={{ name: 'hideSweetAlert', duration: 300 }}
@@ -269,9 +256,105 @@ export default class Inicio extends React.Component {
 					navItems={this.state.loadNavBarItems}
 					title={"Proyecto Communities"}
 				/>
-				<div className="container">
-				
+				<div className="row color-azul-claro padding-top-15">
+					<div className="col-xs-12 col-md-6 banner-inicio-1">
+						<h1>El lugar correcto para aumentar la productividad de tu equipo de trabajo y llevarlo a otro nivel</h1>
+						<img src="http://consultoraambiental.mx//wp-content/uploads/2019/02/performance.png" className="size-img-productivity"/>
+					</div>
+					<div className="col-xs-12 col-md-6">
+						<Chart
+							chartType='Gantt'
+							width={'100%'}
+							height={'200px'}
+							loader={<div>Loading chart</div>}
+							data={[
+								[
+									{ type: 'string', label: 'ID Tarea' },
+									{ type: 'string', label: 'Nombre Tarea' },
+									{ type: 'string', label: 'Recurso' },
+									{ type: 'date', label: 'Fecha Inicio' },
+									{ type: 'date', label: 'Fecha Fin' },
+									{ type: 'number', label: 'Duración' },
+									{ type: 'number', label: 'Percentaje acompletado' },
+									{ type: 'string', label: 'Dependencias' },
+								],
+								[
+									'CrearRequerimientos',
+									'Creación de oficios de requerimientos',
+									'David',
+									new Date(2021, 2, 20),
+									new Date(2021, 2, 21),
+									null,
+									100,
+									null,
+								],
+								[
+									'EntregaRequerimientos',
+									'Entrega de oficios de requerimientos',
+									'David',
+									new Date(2021, 2, 22),
+									new Date(2021, 2, 23),
+									null,
+									100,
+									null,
+								],
+								[
+									'DiseñoLogosEvento',
+									'Diseño de logos del evento',
+									'Carlos',
+									new Date(2021, 2, 22),
+									new Date(2021, 2, 24),
+									null,
+									100,
+									null,
+								],
+								[
+									'ObtenerPonentes',
+									'Obtener Ponentes',
+									'Diana',
+									new Date(2021, 2, 25),
+									new Date(2021, 3, 15),
+									null,
+									57,
+									null,
+								],
+								[
+									'DifusionEvento',
+									'Difusión del evento',
+									'Karen',
+									new Date(2021, 3, 16),
+									new Date(2021, 4, 15),
+									null,
+									0,
+									null,
+								],
+							]}
+							options={{
+								height: 200,
+								backgroundColor: { fill: "#F5F5FF" },
+								gantt: {
+									innerGridHorizLine: {
+										stroke: '#F5F5FF',
+										strokeWidth: 1,
+									},
+									innerGridTrack: { fill: '#F5F5FF' },
+									innerGridDarkTrack: { fill: '#F5F5FF' },
+									trackHeight: 30,
+									labelStyle: { fontName: "arial"}
+								},
+							}}
+							chartLanguage={"es-419"}
+						/>
+					</div>
 				</div>
+				<div className="container">
+					<div className="row margin-top-50">
+						<div className="col-12 text-center">
+							<h1>Tu mandas, tus activdades, tu equipo, tu decides como organizarlo todo</h1>
+						</div>
+					</div>
+				</div>
+				
 				{this.state.flotante}
 			</div>
 		);
